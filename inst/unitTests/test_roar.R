1278770f89f9f8fd68533e4d6e1b7fbe88b787b6
Elena Grassi <grassi.e@gmail.com>
2014-09-18 14:40:25 +0200

    First test for computePairedPvals

--- inst/unitTests/test_roar.R
+++ inst/unitTests/test_roar.R
@@ -387,6 +387,59 @@ test_computePvals_multipleSamples <- function() {
    checkEqualsNumeric(assay(rds@pVals,1)[1,6], 0.01938319, tolerance=1e-5)
 }
 
+test_computePairedPvals <- function() {
+   gene_id <- c("A_PRE", "A_POST")
+   features <- GRanges(
+      seqnames = Rle(c("chr1", "chr1")),
+      strand = strand(rep("+", length(gene_id))),
+      ranges = IRanges(
+         start=c(1000, 2000),
+         width=c(1000, 900)),
+      DataFrame(gene_id)
+   )
+   rds <- new("RoarDataset", treatmentBams=list(), controlBams=list(), 
+              prePostCoords=features, step = 2, cores=1)
+   preElems <- grep("_PRE$", elementMetadata(rds@prePostCoords)$gene_id)
+   postElems <- grep("_POST$", elementMetadata(rds@prePostCoords)$gene_id)
+   preCoords <- rds@prePostCoords[preElems,]
+   se <- SummarizedExperiment(assays = matrix(nrow=1, ncol=4),
+                              rowData=preCoords, 
+                              colData=DataFrame(row.names=c("treatment_pre","treatment_post","control_pre", "control_post"))
+   )
+   
+   rowData(rds) <- rowData(se)
+   colData(rds) <- colData(se)
+   assays(rds) <- assays(se)
+   names(assays(rds)) <- "counts"
+   # We need to set these lengths to choose the treatment branch of the if in computePvals (about number of samples).
+   length(rds@treatmentBams)  <- 3
+   length(rds@controlBams)  <- 2
+   rds@countsTreatment <- SummarizedExperiment(assays = matrix(nrow=1, ncol=2),
+                                               rowData=preCoords, 
+                                               colData=DataFrame(row.names=c("pre","post"))
+   )
+   rds@countsControl <- SummarizedExperiment(assays = matrix(nrow=1, ncol=2),
+                                             rowData=preCoords, 
+                                             colData=DataFrame(row.names=c("pre","post"))
+   )
+   assay(rds@countsTreatment,2) <- matrix(nrow=1,ncol=2)
+   assay(rds@countsTreatment,3) <- matrix(nrow=1,ncol=2)
+   assay(rds@countsControl,2) <- matrix(nrow=1,ncol=2)
+   assay(rds@countsTreatment,1)[1,] <- c(10,10)
+   assay(rds@countsTreatment,2)[1,] <- c(1,10)
+   assay(rds@countsTreatment,3)[1,] <- c(20,10)
+   assay(rds@countsControl,1)[1,] <- c(10,5)
+   assay(rds@countsControl,2)[1,] <- c(10,20)
+   # 2_1 -> 0.00522109
+   # 3_2 -> 0.01938319
+   # We need to setup the second assay that computePVals will fill.
+   assay(rds,2) <- matrix(nrow=1, ncol=4)
+   rds <- computePairedPvals(rds, c(2,3), c(1,2))
+   checkEqualsNumeric(assay(rds@pVals,1)[1,1], 0.00522109, tolerance=1e-5)
+   checkEqualsNumeric(assay(rds@pVals,1)[1,2], 0.01938319, tolerance=1e-5)
+   checkEqualsNumeric(assay(rds,2)[1,4], 0.001032092, tolerance=1e-5)
+}
+
 # GRanges order
 test_computeRoars_singleSamples_GRanges_order <- function() {
    gene_id <- c("B_POST", "A_PRE", "C_POST", "B_PRE", "A_POST", "C_PRE")
