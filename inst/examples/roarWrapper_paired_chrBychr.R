80d2971ac931d5bdf584d998566b8148f49f4ec2
e.grassi <e.grassi@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-09-22 09:03:13 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 7 commits.
    Commit information:
    
    Commit id: d81b0dd3ea95b73074148493c09e0c734384c84b
    
        Fixed man pages and NEWS/DESCRIPTION
    
    Committed by: Elena Grassi
    Author Name: Elena Grassi
    Commit date: 2014-09-22 10:56:04 +0200
    Author date: 2014-09-19 17:36:26 +0200
    
    Commit id: 55474787406c86eb176efea627f74a4b26da60b5
    
        Wrapper script for paired datasets
    
    Committed by: Elena Grassi
    Author Name: Elena Grassi
    Commit date: 2014-09-22 10:56:04 +0200
    Author date: 2014-09-19 14:43:42 +0200
    
    Commit id: 61e3a091be3a88a7c343eba30be5642a2f531786
    
        Structure for new man pages
    
    Committed by: Elena Grassi
    Author Name: Elena Grassi
    Commit date: 2014-09-22 10:56:04 +0200
    Author date: 2014-09-18 18:29:53 +0200
    
    Commit id: 066597ecb64370f42f07d3f86adedc627d565192
    
        Two other tests on results methods
    
    Committed by: Elena Grassi
    Author Name: Elena Grassi
    Commit date: 2014-09-22 10:56:04 +0200
    Author date: 2014-09-18 15:48:50 +0200
    
    Commit id: 1278770f89f9f8fd68533e4d6e1b7fbe88b787b6
    
        First test for computePairedPvals
    
    Committed by: Elena Grassi
    Author Name: Elena Grassi
    Commit date: 2014-09-22 10:56:04 +0200
    Author date: 2014-09-18 14:40:25 +0200
    
    Commit id: 62f3df2a71b9db040240fb44c026f53921d9a8ed
    
        combinedPvals method now runs - still untested
    
    Committed by: Elena Grassi
    Author Name: Elena Grassi
    Commit date: 2014-09-22 10:56:04 +0200
    Author date: 2014-09-18 13:17:32 +0200
    
    Commit id: fe2c2753921ce1a70cc5f5c59f136de389647cc3
    
        Structure for fisher method to combine pvalues
    
           To avoid changing any pre-existing
           signature I added a computePairedPvals
           method (still need to be implemented).
        
    Committed by: Elena Grassi
    Author Name: Elena Grassi
    Commit date: 2014-09-22 10:56:04 +0200
    Author date: 2014-09-17 17:52:50 +0200
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/roar@94354 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/examples/roarWrapper_paired_chrBychr.R
@@ -0,0 +1,121 @@
+#!/usr/bin/env Rscript
+# Script to perform stepwise (chr by chr) Roar analysis. 
+# Requires a gtf with _PRE and _POST gene_ids and bam files from the two 
+# conditions to be compared.
+
+checkReadable <- function(filename) {
+   res <- file.access(names=filename, mode=4) == 0
+   if (!res) {
+      warning(paste(filename, "is not readable", sep=" "))
+   }
+   res
+}
+
+arguments <- matrix(c(
+   'help', 'h', 0, "logical",
+   'debug', 'd', 1, "character",
+   'gtf' , 'a', 1, "character",
+   'treatment'  , 't', 1, "character",
+   'control'  , 'c', 1, "character",
+   't_samples_order', 'o', 1, "character",
+   'c_samples_order', 'p', 1, "character"
+), ncol=4, byrow=T)
+
+library(getopt)
+opt <- getopt(arguments)
+
+if (!is.null(opt$help)) {
+   stop(getopt(arguments, command=get_Rscript_filename(), usage=TRUE))
+}
+
+if (is.null(opt$gtf)) {
+   stop("Missing gtf [-a filename] annotation option\n")
+}
+
+if (is.null(opt$treatment) | is.null(opt$control)) {
+   stop("Missing treatment or control [-t, -c followed by comma separated bam files] param")
+}
+
+if (is.null(opt$t_samples_order) | is.null(opt$c_samples_order)) {
+   stop("Missing treatment or control samples order [-o, -p followed by comma separated bam files] param")
+}
+
+
+library(roar)
+library(rtracklayer)
+library(Rsamtools)
+library(GenomicAlignments)
+
+treatmentBams <- as.vector(unlist(strsplit(opt$treatment, ",")))
+controlBams <- as.vector(unlist(strsplit(opt$control, ",")))
+treatmentSamples <- as.vector(unlist(strsplit(opt$t_samples_order, ",")), mode="integer")
+controlSamples <- as.vector(unlist(strsplit(opt$c_samples_order, ",")), mode="integer")
+
+if (!all(sapply(c(treatmentBams, controlBams, opt$gtf), checkReadable))) {
+   stop("One of the given files does not exist or is not readable")  
+}
+
+gtfGRanges<- import(opt$gtf, asRangedData=FALSE)
+chrs <- seqlevels(gtfGRanges)
+
+orderBam <- function(bam) {
+      tmp <- tempfile()
+      ordered <- sortBam(bam, tmp, byQname=FALSE, maxMemory=512)
+      garbage <- indexBam(ordered)
+      return(ordered)
+}
+
+orderedTreatmentBams <- lapply(treatmentBams, orderBam)
+orderedControlBams <- lapply(controlBams, orderBam)
+
+workOnChr <- function(chr) {
+   write(paste("Working on", chr), stderr())
+   reduced <- keepSeqlevels(gtfGRanges, chr) 
+   coords <- c(start(reduced), end(reduced))  # To keep strandness in consideration! Is this needed?
+   begin <- min(coords)
+   end <- max(coords)
+   spanChr <- GRanges(seqnames=chr,ranges=IRanges(start=begin,width=end-begin+1))
+   loadBam <- function(bam) {
+      param <- ScanBamParam(which=spanChr)
+      res <- readGAlignments(file=bam, param = param)
+      return(res)
+   } 
+
+   treatmentBamsGenomicAlignments <- lapply(orderedTreatmentBams, loadBam)
+   controlBamsGenomicAlignments <- lapply(orderedControlBams, loadBam)
+   
+   rds <- RoarDataset(treatmentBamsGenomicAlignments, controlBamsGenomicAlignments, reduced)
+   
+   # Get counts
+   rds <- countPrePost(rds, FALSE)
+   # Get m/M and Roar
+   rds <- computeRoars(rds)
+   # Fisher test
+   rds <- computePairedPvals(rds, treatmentSamples, controlSamples)
+   res <- countResults(rds)
+   return(res)
+}
+
+allRes <- lapply(chrs, workOnChr)
+meltedRes <- do.call("rbind", allRes)
+preElems <- grep("_PRE$", elementMetadata(gtfGRanges)$gene_id)
+pre <- gtfGRanges[preElems,]
+preLen <- end(pre) - start(pre) + 1
+names <- sub("^\\s+","",sub("_PRE", "",elementMetadata(pre)$gene_id))
+meltedRes <- meltedRes[match(names, rownames(meltedRes)),]
+sumPreTreatment <- sum(meltedRes[,"treatmentValue"])
+sumPreControl <- sum(meltedRes[,"controlValue"])
+meltedRes$treatmentFpkm <- (meltedRes[,"treatmentValue"]*1000000000)/(preLen*sumPreTreatment)
+meltedRes$controlFpkm <- (meltedRes[,"controlValue"]*1000000000)/(preLen*sumPreControl)
+write.table(meltedRes, sep="\t", quote=FALSE)
+
+unlink(orderedTreatmentBams)
+unlink(orderedControlBams)
+treatmentBai <- sub(".bam", ".bai", orderedTreatmentBams)
+controlBai <- sub(".bam", ".bai", orderedControlBams)
+unlink(treatmentBai)
+unlink(controlBai)
+
+if (!is.null(opt$debug)) {
+   save.image(file=opt$debug)
+}
