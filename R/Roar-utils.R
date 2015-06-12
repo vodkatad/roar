fdd083094a05f0bff53d152e2bfb69e3c534f5a2
Elena Grassi <grassi.e@gmail.com>
2015-06-12 15:11:11 +0200

    First try to manage minus strand

--- R/Roar-utils.R
+++ R/Roar-utils.R
@@ -58,8 +58,7 @@ getApaGenesFractions <- function(geneGr, apaGr)
    if (strand == '+') {
       getApaGenesFractionsPlusStrand(geneGr, apaGr, chr, strand, gene_id)
    } else if (strand == '-') {
-      stop("Implementation for minus strand is still missing")
-      # TODO minus varsion
+      getApaGenesFractionsMinusStrand(geneGr, apaGr, chr, strand, gene_id)
    } else {
       stop("A gene has no strand info or both + and -")
    } 
@@ -184,6 +183,128 @@ getApaGenesFractionsPlusStrand <- function(geneGr, apaGr, chr, strand, gene_id)
    return(list(fragments, apaFragmentsPrePost))
 }
 # Testing will be hideous.
+
+# Strand -. We are being not DRY but i prefer to start in this way and refactor
+# after the first runs/tests. During active development I will have to remember
+# to change code (if needed) in two places...but right now I'm not sure
+# about how many differences I need to insert in the code.
+getApaGenesFractionsMinusStrand <- function(geneGr, apaGr, chr, strand, gene_id)
+{   
+   geneGr <- sort(geneGr, decreasing=TRUE)
+   apaGr <- sort(apaGr, decreasing=TRUE)
+   introns <- sort(gaps(geneGr), decreasing=TRUE)
+   # Remove last intron: it is not useful in any way.
+   introns <- head(introns, n=length(introns)-1)
+   lastApa <- tail(apaGr, n=1)
+   names <- unlist(strsplit(mcols(lastApa)$apa, '_', fixed=TRUE))
+   lastApa_name <- names[1]
+   apaFragmentsPrePost <- vector("list", length(apaGr)-1)
+   apaFrI <- 1
+   # We start by doing it iteratively and non R/Bioc stylishly because I've got
+   # no idea how. 
+   # New idea: we combine exons and introns adding them mcols to their kind
+   # and with info about being overlappant or not.
+   # Then we run over this GRanges that should have all the needed info
+   # to generate a GRanges res with intervals and maybe also a list of 
+   # pre/post indexes for every APA?
+   mcols(geneGr) <- NULL
+   mcols(geneGr)$type <- 'e'   
+   if (length(introns) != 0) {
+      mcols(introns)$type <- 'i'  
+   }   
+   whole <- sort(c(geneGr, introns))
+   hits <- findOverlaps(whole, apaGr)
+   # We have downstream apas so we can't do this.
+   #if (!all(countSubjectHits(hits) == 1)) {
+   #   stop("Error: a given apa does not overlap its gene")
+   #}
+   
+   foundov <- length(unique(whole[queryHits(hits)]))
+   mcols(whole)$overlap <- rep(FALSE, length(whole))   
+   mcols(whole[unique(queryHits(hits))])$overlap <- rep(TRUE, foundov)
+   # Growing objects sin.
+   begins <- c()
+   ends <- c()
+   begin <- -1
+   lastExonB <- -1
+   # Write logic and invariant here: begin is always the beginning of the last
+   # needed fragment (i.e. start of exon with an overlap or with an overlap in
+   # the next intron or just seen apa). We add an end when 
+   # we get an exon/intron with overlapping apas, and a begin for exons
+   # with overlap or preceding an overlapping intron.
+   for (i in 1:length(whole)) {
+      if (whole[i]$type=='e') {
+         if (whole[i]$overlap || (i+1<=length(whole) && whole[i+1]$overlap)) {
+            if (begin != -1) {
+               begins <- c(begins, begin)
+               ends <- c(ends, end(whole[i-1]))
+            }
+            begin <- start(whole[i])
+            lastExonB <- length(begins)+1
+         }
+      }
+      if (whole[i]$overlap) {
+         overlapping_apas <- apaGr[subjectHits(hits[queryHits(hits)==i])]
+         # Here we have a shot of writing the pre portions of counts
+         # for each APA.
+         for (j in 1:length(overlapping_apas)) {
+            begins <- c(begins, begin)
+            ends <- c(ends, start(overlapping_apas[j]))
+            # We add 1 to avoid overlapping PRE/POST fragments 
+            # - APA are considered before the cut.
+            # In the previous gtf we skipped the "cut" base altogether.
+            # XXX need to decide.
+            begin <- end(overlapping_apas[j])-1
+            firstApa_name <- unlist(
+               strsplit(mcols(overlapping_apas[j])$apa, '_', fixed=TRUE))[1]
+            apa_name <- paste(firstApa_name, lastApa_name, sep="-")
+            apaFragmentsPrePost[[apaFrI]] <- new("ApaFragmentPrePost",
+                                                 name=apa_name,
+                                                 PREstart=lastExonB, 
+                                                 PREend=length(begins))
+            apaFrI <- apaFrI+1
+         }
+      }
+   }
+   # We could be ok like this (last APA inside an exon, we stop there)
+   # or we could have a last APA after the last exon that we want to consider.
+   #if (an APA outside hits) {
+   if (!all(countSubjectHits(hits) == 1)) {
+      out_apas <- apaGr[countSubjectHits(hits) == 0]
+      # If we did not see an overlap (should be rare) we have a single fragment 
+      # starting at the beginning of the last exon.
+      if (begin == -1) {
+         begin <- start(tail(geneGr, n=1)) 
+         lastExonB <- length(begins)+1 # That's always 1 I know.
+      } 
+      for (j in 1:length(out_apas)) {
+         begins <- c(begins, begin)
+         ends <- c(ends, start(out_apas[j]))
+         # We add 1 to avoid overlapping PRE/POST fragments 
+         # - APA are considered before the cut.
+         # In the previous gtf we skipped the "cut" base altogether.
+         # XXX need to decide.
+         begin <- end(out_apas[j])-1
+         firstApa_name <- unlist(
+            strsplit(mcols(out_apas[j])$apa, '_', fixed=TRUE))[1]
+         apa_name <- paste(firstApa_name, lastApa_name, sep="-")
+         apaFragmentsPrePost[[apaFrI]] <- new("ApaFragmentPrePost",
+                                              name=apa_name,
+                                              PREstart=lastExonB, 
+                                              PREend=length(begins))
+         apaFrI <- apaFrI+1
+      }
+   }
+   fragments <- GRanges(seqnames=chr, strand=strand, 
+                        ranges=IRanges(start=begins, end=ends))
+   # The last apaFragment is build from a single apa only and we don't want it
+   #fragments <- head(fragments, n=length(fragments)-1)
+   # But the fragment is ok!
+   apaFragmentsPrePost <- head(apaFragmentsPrePost,
+                               n=apaFrI-2)
+   return(list(fragments, apaFragmentsPrePost))
+}
+
 
 obtainPrePost <- function(prepost, fragments)
 {
