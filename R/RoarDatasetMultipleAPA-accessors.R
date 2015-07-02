acd89d62966d0f7a0556b22b0b061dd96b295767
Elena Grassi <grassi.e@gmail.com>
2015-07-02 16:03:54 +0200

    First naive test for costructors

--- R/RoarDatasetMultipleAPA-accessors.R
+++ R/RoarDatasetMultipleAPA-accessors.R
@@ -1,23 +1,44 @@
 RoarDatasetMultipleAPAFromFiles <- function(treatmentBams, controlBams, gtf) {
-   gtfGRanges<- import(gtf, asRangedData=FALSE)
+   gtfGRanges <- import(gtf, asRangedData=FALSE)
+   #apas_melted <- gtfGRanges[mcols(gtfGRanges)$type=="apa"]
+   #genes_melted <- gtfGRanges[mcols(gtfGRanges)$type=="gene"]
+   #mcols(apas_melted)$gene <- sapply(strsplit(mcols(apas_melted)$apa, '_', 
+   #                                           fixed=TRUE),
+   #                           function(x) { x[length(x)]})
+   #genes_ids <- sort(unique(mcols(genes_melted)$gene))
+   #genes_ids_apas <- sort(unique(as.numeric(mcols(apas_melted)$gene)))
+   #if (!all(genes_ids==genes_ids_apas)) {
+   #   stop("All the genes in the gtf should have at least one apa")
+   #}
+   #genes <- do.call(GRangesList, sapply(genes_ids, 
+   #                  function(x) {genes_melted[mcols(genes_melted)$gene==x]}))
+   #apas <- do.call(GRangesList, sapply(genes_ids, 
+   #                  function(x) {apas_melted[mcols(apas_melted)$gene==x]}))
+   #names(apas) <- genes_ids
+   #names(genes) <- genes_ids
+   treatmentBamsGenomicAlignments <- lapply(treatmentBams, readGAlignments)
+   controlBamsGenomicAlignments <- lapply(controlBams, readGAlignments)
+   RoarDatasetMultipleAPA(treatmentBamsGenomicAlignments, controlBamsGenomicAlignments, gtfGRanges)
+}
+
+
+RoarDatasetMultipleAPA <- function(treatmentBamsGenomicAlignments, controlBamsGenomicAlignments, gtfGRanges) {
    apas_melted <- gtfGRanges[mcols(gtfGRanges)$type=="apa"]
    genes_melted <- gtfGRanges[mcols(gtfGRanges)$type=="gene"]
    mcols(apas_melted)$gene <- sapply(strsplit(mcols(apas_melted)$apa, '_', 
                                               fixed=TRUE),
-                              function(x) { x[length(x)]})
-   genes_ids <- sort(unique(mcols(genes_melted)$gene))
-   genes_ids_apas <- sort(unique(as.numeric(mcols(apas_melted)$gene)))
+                                     function(x) { x[length(x)]})
+   genes_ids <- sort(unique(as.character(mcols(genes_melted)$gene)))
+   genes_ids_apas <- sort(unique(mcols(apas_melted)$gene))
    if (!all(genes_ids==genes_ids_apas)) {
       stop("All the genes in the gtf should have at least one apa")
    }
    genes <- do.call(GRangesList, sapply(genes_ids, 
-                     function(x) {genes_melted[mcols(genes_melted)$gene==x]}))
+                                        function(x) {genes_melted[mcols(genes_melted)$gene==x]}))
    apas <- do.call(GRangesList, sapply(genes_ids, 
-                     function(x) {apas_melted[mcols(apas_melted)$gene==x]}))
+                                       function(x) {apas_melted[mcols(apas_melted)$gene==x]}))
    names(apas) <- genes_ids
    names(genes) <- genes_ids
-   treatmentBamsGenomicAlignments <- lapply(treatmentBams, readGAlignments)
-   controlBamsGenomicAlignments <- lapply(controlBams, readGAlignments)
    new("RoarDatasetMultipleAPA", treatmentBams=treatmentBamsGenomicAlignments, 
        controlBams=controlBamsGenomicAlignments, 
        geneCoords=genes, apaCoords=apas, step=0, paired=FALSE, cores=1)
---
+++ inst/unitTests/test_roar_multipleAPA.R
@@ -0,0 +1,24 @@
+# To manually perform single tests: source("inst/unitTests/test_roar.R")
+# To perform all tests: 
+# library("GenomicAlignments")
+# BiocGenerics:::testPackage("roar")
+
+test_RoarDatasetMultipleAPA_error_mismatch_gene_apa <- function() {
+   gene <- c("A", "B", NA)
+   type <- c("gene","gene","apa")
+   apa <- c(NA, NA, "apa1_A")
+   features <- GRanges(
+      seqnames = Rle(c("chr1", "chr2", "chr1")),
+      strand = strand(rep("+", length(gene))),
+      ranges = IRanges(
+         start=c(1000, 2000, 1300),
+         width=c(500, 900, 1)),
+      DataFrame(gene, apa, type)
+   )
+   
+   rd1 <- GAlignments("a", seqnames = Rle("chr1"), pos = as.integer(1000), cigar = "300M", strand = strand("+"))
+   obs <- tryCatch(RoarDatasetMultipleAPA(list(c(rd1,rd1)), list(c(rd1,rd1)), features), 
+                   error=function(e) e)
+   checkEquals(obs$message, "All the genes in the gtf should have at least one apa")
+}
+
