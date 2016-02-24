bb9d80dd1b8da4efe086394fb5e79207d68e6ccc
e.grassi <e.grassi@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-02-24 16:31:44 +0000

    Integrative test for RoarDatasetMultipleAPA up to countPrePost
    Also added comments to results man pages still to be fixed.
    
    From: Elena Grassi <data@clo>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/roar@113991 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_roar_multipleAPA.R
+++ tests/testthat/test_roar_multipleAPA.R
@@ -1,4 +1,5 @@
 test_that("test_getApaGenesFractionsPlusStrandBasic", {
+    # Here we put two APA inside the first and last exon of a three exon gene.
     chr <- rep("chr1", 3)
     strand <- rep("+", 3)
     gene <- rep("A", 3)
@@ -13,11 +14,16 @@ test_that("test_getApaGenesFractionsPlusStrandBasic", {
         "apa2_"), gene[1]), type = rep("apa", 2)))
     res <- getApaGenesFractionsPlusStrand(geneGr, apas, chr[1], 
         strand[1], gene[1])
-    expect_equal(2, length(res))
+    # Two possible apa choices.
+    expect_equal(2, length(res)) 
+    # Four fragments.
     expect_equal(4, length(res[[1]]))
+    # Two info for which fragments need to be considered for the two choices.
     expect_equal(2, length(res[[2]]))
+    # For the first APA choice we sum for PRE only the first fragment counts.
     expect_equal(1, res[[2]][[1]]@PREstart)
     expect_equal(1, res[[2]][[1]]@PREend)
+    # For the second one up to the third (for POST we get all the remaining fragments counts).
     expect_equal(3, res[[2]][[2]]@PREstart)
     expect_equal(3, res[[2]][[2]]@PREstart)
     expect_equal("apa2", res[[2]][[2]]@name)
@@ -101,5 +107,63 @@ test_that("test_RoarDatasetMultipleAPA_error_mismatch_gene_apa",
             list(c(rd1, rd1)), features), error = function(e) e)
         expect_equal("All the genes in the gtf should have at least one apa", 
             obs$message)
+    })
+
+test_that("test_badIntegrationTest", 
+    {
+       chr <- rep("chr1", 3)
+       strand <- rep("+", 3)
+       gene <- rep("A", 3)
+       type <- rep("gene", 3)
+       apa <- rep(NA, 3)
+       geneGr <- GRanges(seqnames = chr, strand = strand, ranges = IRanges(start = c(1000, 
+                                                                                     2000, 3300), width = c(500, 900, 10)), DataFrame(gene, apa, type))
+       gene2 <- rep("B", 3)
+       chr2 = rep("chr2", 3)
+       geneGr2 <- GRanges(seqnames = chr2, strand = strand, ranges = IRanges(start = c(1000, 
+                                                                                       2000, 3300), width = c(500, 900, 10)), DataFrame(gene=gene2, apa, type))
+       apas1 <- GRanges(seqnames = c(chr[1], chr[1]), strand = c(strand[1], strand[1]), 
+                        ranges = IRanges(start = c(1100, 3305), width = c(1, 1)), 
+                        DataFrame(gene = rep(gene[1], 2), apa = paste0(c("apa1_", "apa2_"), gene[1]), type = rep("apa", 2)))
+       apas2 <- GRanges(seqnames = c(chr2[1], chr2[1]), strand = c(strand[1], strand[1]), 
+                        ranges = IRanges(start = c(1100, 3305), width = c(1, 1)), 
+                        DataFrame(gene = rep(gene2[1], 2), apa = paste0(c("apa1_", "apa2_"), gene2[1]), type = rep("apa", 2)))
+       
+       # We have two "identical" genes with the same two apa as the first test on chr1 and chr2
+       features <- unlist(GRangesList(geneGr, geneGr2, apas1, apas2)) 
+       # c() was complaining for disjoint chr (seqinfo)
+       
+       # FIXME if cigar=300M nothing was seen!
+       rd1 <- GAlignments("a", seqnames = Rle("chr1"), pos = as.integer(3301), 
+                          cigar = "1M", strand = strand("+"))
+       rd2 <- GAlignments("a", seqnames = Rle("chr1"), pos = as.integer(3301), 
+                          cigar = "1M", strand = strand("-"))
+       rd3 <- GAlignments("a", seqnames = Rle("chr1"), pos = as.integer(3301), 
+                          cigar = "1M", strand = strand("+"))
+       rd4 <- GAlignments("a", seqnames = Rle("chr1"), pos = as.integer(3306), 
+                          cigar = "1M", strand = strand("+"))
+       rd5 <- GAlignments("a", seqnames = Rle("chr2"), pos = as.integer(3306), 
+                          cigar = "1M", strand = strand("+"))
+       rd6 <- GAlignments("a", seqnames = Rle("chr2"), pos = as.integer(3306), 
+                          cigar = "1M", strand = strand("-"))
+       rd7 <- GAlignments("a", seqnames = Rle("chr2"), pos = as.integer(3306), 
+                          cigar = "1M", strand = strand("+"))
+       rd8 <- GAlignments("a", seqnames = Rle("chr2"), pos = as.integer(3301), 
+                          cigar = "1M", strand = strand("+"))
+       
+       reads <- suppressWarnings(list(c(rd1, rd2, rd3, rd4, rd5, rd6, rd7, rd8))) 
+       # suppressWarnings for the same issue about different chr as before.
+       # We have 3 reads on the PRE of the second choice APA and 1 on the post for gene A and the opposite for gene B.
+       rds <- RoarDatasetMultipleAPA(reads, reads, features)
+       rds <- countPrePost(rds)
+       counts_geneA = matrix(c(0, 4, 0, 4, 3, 1, 3, 1), byrow = TRUE, ncol = 4)
+       colnames(counts_geneA) = c("treatment_pre","treatment_post","control_pre","control_post")
+       expect_equal(counts_geneA, assay(rds@roars[[1]],1))
+       counts_geneB = matrix(c(0, 4, 0, 4, 1, 3, 1, 3), byrow = TRUE, ncol = 4)
+       colnames(counts_geneB) = c("treatment_pre","treatment_post","control_pre","control_post")
+       expect_equal(counts_geneB, assay(rds@roars[[2]],1))
+       #rds <- computeRoars(rds)
+       #rds <- computePvals(rds)
+       #res <- countResults(rds)
     })
 
